c6157ccae569653e252d8d3952ffb8f5fabb8423
Russell Moore <russellwmoore@gmail.com>
2019-10-09 12:34:43 -0400

    remove comments from times

--- questions/06_times/times.js
+++ questions/06_times/times.js
@@ -1,12 +1,3 @@
-/*
-const tenTimes = times(10);
-console.log(typeof tenTimes);// logs function
-console.log(tenTimes(5));// logs 50
-const fiveTimes = times(5);
-console.log(fiveTimes(5));//logs 25
-const badTimes = times('x');//throw an exception
-*/
-
 function times() {}
 
 module.exports = { times };
